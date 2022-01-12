b637d349025fa8db2afdca3dd9b82b402e197225
Matt Czencz <Mattczencz24@gmail.com>
2022-01-12 00:01:04 -0700

    Added switch block functionality, added conditional logic to check if the blocks match the switch type and to do something for each scenario

--- Functions Game/Assets/Scripts/Player/BlockDetection.cs
+++ Functions Game/Assets/Scripts/Player/BlockDetection.cs
@@ -9,15 +9,21 @@ public class BlockDetection : MonoBehaviour
 
     // Accessing Player Inventory Script
     public PlayerInventory playerInventory;
+    public PressurePlateActiveCheck currentPressurePlate;
 
-    // Private collider to set the tag of what you just walked into
+    // Private colliders to set the tag of what you just walked into
     public Collider nearbyBlock;
+    public Collider nearbySwitch;
 
     // Private String variables to hold the tag names
     string dtString = "DT_String";
     string dtBool = "DT_Bool";
     string dtInt = "DT_Int";
     string dtFloat = "DT_Float";
+    string ppString = "PP_String";
+    string ppBool = "PP_Bool";
+    string ppInt = "PP_Int";
+    string ppFloat = "PP_Float";
 
     // Happens every frame
     private void Update()
@@ -27,6 +33,11 @@ public class BlockDetection : MonoBehaviour
         {
             // Run the TagCheck function
             TagCheck(nearbyBlock);
+        }
+
+        if (nearbySwitch != null)
+        {
+            PressurePlateCheck(nearbySwitch);
         }
     }
 
@@ -39,6 +50,14 @@ public class BlockDetection : MonoBehaviour
             nearbyBlock = other;
             Debug.Log("Can I interact? " + charCont.canInteract);
         }
+
+        if (other.tag == ppString || other.tag == ppBool || other.tag == ppFloat || other.tag == ppInt)
+        {
+            charCont.canInteract = true;
+            currentPressurePlate = other.GetComponent<PressurePlateActiveCheck>();
+            nearbySwitch = other;
+            Debug.Log(nearbySwitch.tag);
+        }
     }
 
     // Only happens when the object exits the trigger area
@@ -49,6 +68,13 @@ public class BlockDetection : MonoBehaviour
             charCont.canInteract = false;
             nearbyBlock = null;
             Debug.Log("Can I interact? " + charCont.canInteract);
+        }
+
+        if (other.tag == ppString || other.tag == ppBool || other.tag == ppFloat || other.tag == ppInt)
+        {
+            currentPressurePlate = null;
+            charCont.canInteract = false;
+            nearbySwitch = null;
         }
     }
 
@@ -71,7 +97,7 @@ public class BlockDetection : MonoBehaviour
             if (charCont.canInteract == true && Input.GetKeyDown(KeyCode.F))
             {
                 playerInventory.hasBoolBlock = true;
-                Debug.Log("I just picked up the " + col.tag + " block! " + playerInventory.hasStringBlock);
+                Debug.Log("I just picked up the " + col.tag + " block! " + playerInventory.hasBoolBlock);
                 Destroy(col.gameObject);
             }
         }
@@ -80,7 +106,7 @@ public class BlockDetection : MonoBehaviour
             if (charCont.canInteract == true && Input.GetKeyDown(KeyCode.F))
             {
                 playerInventory.hasIntBlock = true;
-                Debug.Log("I just picked up the " + col.tag + " block! " + playerInventory.hasStringBlock);
+                Debug.Log("I just picked up the " + col.tag + " block! " + playerInventory.hasIntBlock);
                 Destroy(col.gameObject);
             }
         }
@@ -89,8 +115,38 @@ public class BlockDetection : MonoBehaviour
             if (charCont.canInteract == true && Input.GetKeyDown(KeyCode.F))
             {
                 playerInventory.hasFloatBlock = true;
-                Debug.Log("I just picked up the " + col.tag + " block! " + playerInventory.hasStringBlock);
+                Debug.Log("I just picked up the " + col.tag + " block! " + playerInventory.hasFloatBlock);
                 Destroy(col.gameObject);
+            }
+        }
+    }
+
+    private void PressurePlateCheck(Collider col)
+    {
+        if (col.tag == ppString)
+        {
+            // If the character has the correct block & the plate is not active yet
+            if (charCont.canInteract == true && Input.GetKeyDown(KeyCode.F) && playerInventory.hasStringBlock == true && currentPressurePlate.isActive == false)
+            {
+                currentPressurePlate.isActive = true;
+                playerInventory.hasStringBlock = false;
+                col.transform.Find("Block").gameObject.SetActive(true);
+                Debug.Log("You placed the " + col.tag + " block!");
+            }
+            // If the character tries to put a block on an already active switch
+            else if (charCont.canInteract == true && Input.GetKeyDown(KeyCode.F) && currentPressurePlate.isActive == true)
+            {
+                Debug.Log("This switch is already activated");
+            }
+            // If the character has the wrong blocks and tries to add it to this one
+            else if (charCont.canInteract == true && Input.GetKeyDown(KeyCode.F) && (playerInventory.hasBoolBlock == true || playerInventory.hasFloatBlock == true || playerInventory.hasIntBlock == true))
+            {
+                Debug.Log("You have the wrong block, try putting that elsewhere.");
+            }
+            // If the character doesn't have the block
+            else if (charCont.canInteract == true && Input.GetKeyDown(KeyCode.F) && playerInventory.hasStringBlock == false)
+            {
+                Debug.Log("You don't have this block! Go grab it.");
             }
         }
     }
---
+++ Functions Game/Assets/Scripts/Pressure Plates/PressurePlateActiveCheck.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateActiveCheck : MonoBehaviour
+{
+    public bool isActive = false;
+}
