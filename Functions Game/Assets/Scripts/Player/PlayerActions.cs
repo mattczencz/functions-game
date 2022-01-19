6dea9cd4285bb75861700ed75c2851043f064c1d
Matt Czencz <Mattczencz24@gmail.com>
2022-01-18 18:05:02 -0700

    Added functionality to stop player movement and return to idle state if they are interacting with an NPC

--- Functions Game/Assets/Scripts/Player/PlayerActions.cs
+++ Functions Game/Assets/Scripts/Player/PlayerActions.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Invector.vCharacterController;
 
 public class PlayerActions : MonoBehaviour
 {
     public bool canInteract = false;
     public bool interacting = false;
 
-    void Update() {
-        if(interacting == true) {
-            Debug.Log("Interacting...");
+    void Update()
+    {
+        if (interacting == true)
+        {
+            GetComponent<vThirdPersonInput>().enabled = false;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+            GetComponent<Animator>().SetFloat("InputMagnitude", 0);
+        }
+        else
+        {
+            GetComponent<vThirdPersonInput>().enabled = true;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
         }
     }
 }
