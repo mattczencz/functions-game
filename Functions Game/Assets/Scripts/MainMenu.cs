12d099d927a9957dc1fe8b8116dbf204e69b7ed1
Matt Czencz <Mattczencz24@gmail.com>
2022-01-25 16:07:59 -0700

    Added main menu and credits scene, fixed issue where mouse was invisible during credit scene

---
+++ Functions Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
